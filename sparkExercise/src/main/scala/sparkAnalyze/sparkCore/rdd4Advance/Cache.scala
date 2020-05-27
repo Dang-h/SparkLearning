d2aebfdcc9f0af92160e7003f30799a7d7c0afef
D_h <54danghao@gmail.com>
2020-05-28 01:13:31 +0800

    SparkCore

--- sparkExercise/src/main/scala/sparkAnalyze/sparkCore/rdd4Advance/Cache.scala
+++ sparkExercise/src/main/scala/sparkAnalyze/sparkCore/rdd4Advance/Cache.scala
@@ -3,17 +3,7 @@ package sparkAnalyze.sparkCore.rdd4Advance
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-/**
- *
- * FUNCTIONAL_DESCRIPTION:
- * CREATE_BY: 尽际
- * CREATE_TIME: 2019/2/27 18:32
- * MODIFICATORY_DESCRIPTION:
- * MODIFY_BY:
- * MODIFICATORY_TIME:
- * VERSION：V1.0
- */
-object Chapter6_1_2_2 {
+object Cache {
 	def main(args: Array[String]): Unit = {
 		val conf = new SparkConf()
 		  .setMaster("local[*]")
@@ -21,7 +11,7 @@ object Chapter6_1_2_2 {
 		val sc = new SparkContext(conf)
 
 		// 数据格式：访问时间， 用户id， 查询词， 该URL在返回结果中的排名， 用户点击的顺序号， 用户点击的URL
-		val sogouRDD = sc.textFile("hdfs://hadoop100:9000/user/test/SogouQ1.txt", 8)
+		val sogouRDD = sc.textFile("data/practice/SogouQ1.txt", 8)
 //		val rddData1 = sogouRDD.map(t => (t.split("\t")(1), 1)) //(user_ID, 1)
 		val rddData1 = sogouRDD.map(t => (t.split("\t")(5), 1)) //(URL, 1)
 //		val rddURL = rddData1.map(c => (c._1.split("((?<=\\/\\/www\\.)[^\\/\\:]+|(?<=\\/\\/)(?!www\\.)[^\\/\\:]+)")(1), 1))
