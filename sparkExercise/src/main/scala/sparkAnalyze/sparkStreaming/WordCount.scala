d66b75c3c939884138d2c93fde6ba0d74b980251
D_h <54danghao@gmail.com>
2020-05-30 17:58:07 +0800

    格式修改

--- sparkExercise/src/main/scala/sparkAnalyze/sparkStreaming/WordCount.scala
+++ sparkExercise/src/main/scala/sparkAnalyze/sparkStreaming/WordCount.scala
@@ -7,9 +7,7 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 object WordCount {
 	def main(args: Array[String]): Unit = {
 
-		val ssc = new StreamingContext(new SparkContext(new SparkConf().setMaster("local[4]").setAppName
-		("StreamingWordCount")),
-			Seconds(5))
+		val ssc = new StreamingContext(new SparkContext(new SparkConf().setMaster("local[4]").setAppName("StreamingWordCount")), Seconds(5))
 
 		// 通过监听netcat窗口创建DStream，读取命令窗口一行行数据
 		val lineStream = ssc.socketTextStream("hadoop100", 9999)
